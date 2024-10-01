324d416f940beed6743502d77e27bdac42ed9917
Makeen Khattab <126344889+Makeen-khattab@users.noreply.github.com>
2024-10-01 17:45:13 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <openssl/sha.h>
+
+
+
+// Compiler View
+
+// This File has no configuration as a result it needs only Native Complier.
+
+
+// Define Dependencies 
+
+// We need openssl as dependencies. https://github.com/openssl/openssl/blob/master/include/openssl/sha.h
+
+
+
+int main() {
+    const std::string message = "Hello World";
+    
+    // Create a buffer to hold the hash
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    
+    // Compute the SHA256 hash
+    SHA256(reinterpret_cast<const unsigned char*>(message.c_str()), message.length(), hash);
+    
+    // Print the hash in hexadecimal format
+    std::cout << "SHA-256 hash of \"" << message << "\" is: ";
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+        printf("%02x", hash[i]);
+    }
+    std::cout << std::endl;
+    
+    return 0;
+}
+
